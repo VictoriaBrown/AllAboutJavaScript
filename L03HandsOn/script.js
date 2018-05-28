e4c369f511b3e4315ce248f64be51693b19e5b54
Victoria Brown <victoriabrown0710@gmail.com>
2018-05-28 11:22:26 -0500

    Create script.js

---
+++ L03HandsOn/script.js
@@ -0,0 +1,22 @@
+let highSchoolGrade = 11;
+let yourGrade;
+
+switch(highSchoolGrade){
+  case 9:
+    yourGrade = "Freshman";
+    break;
+  case 10:
+    yourGrade = "Sophomore";
+    break;
+  case 11:
+    yourGrade = "Junior";
+    break;
+  case 12: 
+    yourGrade = "Senior";
+    break;
+  default:
+    yourGrade = "Invalid";
+}
+
+let firstName;
+const name = firstName === "John" ? document.getElementById("demo").innerHTML = "Hello John!" : document.getElementById("demo").innerHTML = "Hello Human!";
