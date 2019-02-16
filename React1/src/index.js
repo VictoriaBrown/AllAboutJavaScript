36bd28e06976049843a8bbe1a2ed80f8fc37dc83
Victoria Brown <victoriabrown0710@gmail.com>
2019-02-16 16:44:08 -0600

    Adding React1

---
+++ React1/src/index.js
@@ -0,0 +1,88 @@
+// Import statements:
+// React is needed to create virtual DOM elements
+import React from 'react';
+// ReactDOM is needed to update the view upon request
+import ReactDOM from 'react-dom';
+
+// Using ReactDOM to update page to include h1 element inside of root element
+ReactDOM.render(
+    // Creating virtual DOM h1 element with 0 props, and child of "Hello, World!"
+    React.createElement("h1", {}, "Hello, World!"),
+    // This is where the h1 element is going to be placed
+    document.getElementById("root")
+);
+
+// Putting an h1 element inside of a div
+ReactDOM.render(
+    React.createElement("div", {}, React.createElement("h1", {}, "h1 element inside of a div")),
+    document.getElementById("root")
+);
+
+// Rendering a new element with a class id being passed in the props
+ReactDOM.render(
+    React.createElement("h1", { className: 'flowers' }, "h1 element with class name being set in props!"),
+    document.getElementById("root")
+);
+
+// Rendering a new anchor element with href being passed in the props
+ReactDOM.render(
+    React.createElement("a", { href: 'https://www.google.com' }, "Click here to go to Google. Anchor element with href being set in props!"),
+    document.getElementById('root')
+);
+
+// Rendering an unordered list
+ReactDOM.render(
+    React.createElement("ul", {}, 
+        React.createElement("li", {}, "Apples"),
+        React.createElement("li", {}, "Bananas"),
+        React.createElement("li", {}, "Mangos")
+    ),
+    document.getElementById("root")
+);
+
+// Rendering a nested unordered list
+ReactDOM.render(
+    React.createElement("ul", {}, 
+        React.createElement("li", {}, 
+            "Apples",
+            React.createElement("ul", {},
+                React.createElement("li", {}, "$1.00 per lb")    
+            ),
+        ),
+        React.createElement("li", {}, 
+            "Oranges",
+            React.createElement("ul", {}, 
+                React.createElement("li", {}, "$.70 per lb")
+            ),
+        ),
+        React.createElement("li", {}, 
+            "Mangos",
+            React.createElement("ul", {}, 
+                React.createElement("li", {}, "$2.00 each")
+            ),
+        )
+    ),
+    document.getElementById("root")
+);
+
+// Breaking down the above nested lists into variables for simplfication
+const applesPrice = React.createElement('li', {}, '$1.00 per lb');
+const apples = React.createElement('ul', {}, applesPrice);
+
+const orangesPrice = React.createElement('li', {}, '$.70 per lb');
+const oranges = React.createElement('ul', {}, orangesPrice);
+
+const mangosPrice = React.createElement('li', {}, '$2.00 each');
+const mangos = React.createElement('ul', {}, mangosPrice);
+
+ReactDOM.render(
+    React.createElement(
+        "ul",
+        {},
+        React.createElement("li", {}, 'Apples', apples),
+        React.createElement("li", {}, 'Oranges', oranges),
+        React.createElement("li", {}, 'Mangos', mangos)
+    ),
+    document.getElementById("root")
+);
+
